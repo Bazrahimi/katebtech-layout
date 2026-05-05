27b7b36eb5fbd5898bb3ebbbd24bee4ede3262f2
Baz Rahimi <bazrahimi@hotmail.com>
2026-05-05 19:39:11 +1000

    add image meta

---
+++ src/images/definitions.ts
@@ -0,0 +1,10 @@
+/**
+ * Image metadata used for rendering images across the app.
+ *
+ * @property url - Image source URL or path.
+ * @property alt - Accessible alternative text describing the image.
+ */
+export type ImageMeta = {
+  url: string;
+  alt: string;
+};
